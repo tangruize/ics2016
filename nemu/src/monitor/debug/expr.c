d24b090b523896cea94a4c49d342b4aa1584d406
tracer-ics2016 <tracer@njuics.org>
2016-09-07 20:08:11 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  20:08:11 up 1 day, 12:51,  2 users,  load average: 0.05, 0.14, 0.13 c0ba51dd6b8a05831cd1195be3f524dec3fb80a9

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -194,7 +194,7 @@ int eva(int p, int q, int sum) {
         {
             int b=strtol(tokens[p+1].str,NULL,0);
             p+=2;
-            printf("b: %d\tsum: %d\n", b,sum);
+            printf("b: %d\tsum: %d\tp: %d\tq: %d\n", b,sum,p,q);
             switch (tokens[p-1].type)
             {
                 case RULE_ADD:return eva(p,q,sum+b);
