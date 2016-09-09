5f3bf4437e40bf294e27b68deab72c893ea51a1c
tracer-ics2016 <tracer@njuics.org>
2016-09-09 17:00:42 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  17:00:42 up 3 days,  9:44,  2 users,  load average: 0.56, 0.84, 1.01 f35be53bd6ed69e623462fc960633d624199dac1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -9,16 +9,33 @@
 #include <regex.h>
 
 enum {
-  RULE_DIGIT=256, RULE_ALPHA, RULE_REG, RULE_NOTYPE,
-  RULE_ADD, RULE_SUB, RULE_MUL, RULE_DIV,
-  RULE_BRA_L, RULE_BRA_R, 
-  RULE_NE, RULE_EQ, RULE_ASSIGN,
-  RULE_OR, RULE_AND, RULE_NOT,
-  RULE_DER, RULE_NEG
+  
+  RULE_ASSIGN=0,
+  RULE_AND, RULE_OR,
+  RULE_EQ, RULE_NE,
+  RULE_ADD, RULE_SUB,
+  RULE_MUL, RULE_DIV,
+  RULE_NOT, RULE_NEG, RULE_DER, 
+  RULE_BRA_L, RULE_BRA_R,
+  
+  RULE_NOTYPE=256, RULE_DIGIT, RULE_ALPHA, RULE_REG
   
   /* done */
   /* TODO: Add more token types */
   
+};
+
+static struct precedence {
+  int rule;
+  int pre;
+} rule_pre[] = {
+  {RULE_ASSIGN,0},
+  {RULE_AND,1}, {RULE_OR,1},
+  {RULE_EQ,2}, {RULE_NE,2},
+  {RULE_ADD,3}, {RULE_SUB,3},
+  {RULE_MUL,4}, {RULE_DIV,4},
+  {RULE_NOT,5}, {RULE_NEG, 5}, {RULE_DER, 5},
+  {RULE_BRA_L,6},{RULE_BRA_R,6}
 };
 
 static struct rule {
@@ -471,7 +488,110 @@ static bool make_token(char *e) {
   return true; 
 }
 
+static int find_digit(int p, int q, int op, int *pre, int *next) {
+  int i;
+  if (tokens[op].type<RULE_NOT) {
+    for (i=op;i>=p;--i) {
+      if (tokens[i].type>RULE_NOTYPE) {
+	*pre=i;
+	break;
+      }
+    }
+    if (i<p) {
+      return -1;
+    }
+  }
+  for (i=op;i<=q;++i) {
+    if (tokens[i].type>RULE_NOTYPE) {
+      *next=i;
+      break;
+    }
+  }
+  if (i>q) {
+    return -1;
+  }
+  return 0;
+}
 
+static int eval(int p, int q, bool *success) {
+  if (p==q) {
+    return tokens[p].value;
+  }
+  int i=p;
+  int op=0;
+  for (i=p;i<=q;++i) {
+    if (tokens[i].type<RULE_NOTYPE) {
+      ++op;
+    }
+  }
+  int j=0;
+  for (;j<op;++j) {
+    int max_pre=-1, max_pre_pos=-1;
+    for (i=p;i<=q;++i) {
+      if (tokens[i].type<RULE_NOTYPE&&rule_pre[tokens[i].type].pre>max_pre) {
+	max_pre_pos=i;
+      }
+    }
+    if (max_pre!=-1) {
+      int pre,next;
+      if (find_digit(p,q,max_pre_pos,&pre,&next)==0) {
+	switch (tokens[max_pre_pos].type) {
+	  case RULE_AND:
+	    tokens[max_pre_pos].value=(tokens[pre].value&&tokens[next].value);
+	    break;
+	  case RULE_OR:
+	    tokens[max_pre_pos].value=(tokens[pre].value||tokens[next].value);
+	    break;
+	  case RULE_EQ:
+	    tokens[max_pre_pos].value=(tokens[pre].value==tokens[next].value);
+	    break;
+	  case RULE_NE:
+	    tokens[max_pre_pos].value=(tokens[pre].value!=tokens[next].value);
+	    break;
+	  case RULE_ADD:
+	    tokens[max_pre_pos].value=(tokens[pre].value+tokens[next].value);
+	    break;
+	  case RULE_SUB:
+	    tokens[max_pre_pos].value=(tokens[pre].value-tokens[next].value);
+	    break;
+	  case RULE_MUL:
+	    tokens[max_pre_pos].value=(tokens[pre].value*tokens[next].value);
+	    break;
+	  case RULE_DIV:
+	    if (tokens[next].value) {
+	      tokens[max_pre_pos].value=(tokens[pre].value/tokens[next].value);
+	    }
+	    else {
+	      fputs("Divisor cannot be zero!\n", stderr);
+	    }
+	    break;
+	  case RULE_NOT:
+	    tokens[max_pre_pos].value=(!tokens[next].value);
+	    break;
+	  case RULE_NEG:
+	    tokens[max_pre_pos].value=(-tokens[next].value);
+	    break;
+	  case RULE_DER:
+	    tokens[max_pre_pos].value=(long)swaddr_read((swaddr_t)tokens[next].value,4);
+	    break;
+	}
+      }
+      tokens[max_pre_pos].type=RULE_DIGIT;
+      tokens[next].type=RULE_NOTYPE;
+      if (tokens[max_pre_pos].type<RULE_NOT) {
+	tokens[pre].type=RULE_NOTYPE;
+      }
+    }
+  }
+  for (i=p;i<=q;++i) {
+    if (tokens[i].type>RULE_NOTYPE) {
+      *success=true;
+      return tokens[i].value;
+    }
+  }
+  *success=false;
+  return -1;
+}
 /*
  * typedef struct split_expr {
  *    int start,end;
@@ -602,6 +722,10 @@ uint32_t expr(char *e, bool *success) {
   if(!make_token(e)) {
     *success = false;
     return 0;
+  }
+  int result=eval(0,nr_token-1,success);
+  if (*success) {
+    printf("%d\n", result);
   }
   // printf("%d\n", eva(0,nr_token-1,0));
   
