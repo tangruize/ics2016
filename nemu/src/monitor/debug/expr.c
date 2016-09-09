26a833e4f8ef7a2776d382d3a5fc042d23190c44
tracer-ics2016 <tracer@njuics.org>
2016-09-10 00:06:56 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  00:06:56 up 3 days, 16:50,  2 users,  load average: 1.61, 1.28, 1.17 5d2f213038f9bf0813876362bc58eb32dd48c40e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -664,7 +664,7 @@ static bool make_token(char *e, bool *is_match) {
       }
     }
     if(i == NR_REGEX) {
-      //printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       is_match=false;
       return false;
     }
