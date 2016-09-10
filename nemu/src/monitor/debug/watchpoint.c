6bd41928a6e4fb8c2a51e8f711161db0f7ed4dba
tracer-ics2016 <tracer@njuics.org>
2016-09-10 13:26:25 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  13:26:25 up 4 days,  6:10,  2 users,  load average: 0.82, 1.14, 1.13 29006ac5f4c1f17037594c1be1e413215b8b1

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -7,59 +7,67 @@ static WP wp_pool[NR_WP];
 WP *head, *free_;
 
 void init_wp_pool() {
-	int i;
-	for(i = 0; i < NR_WP; i ++) {
-		wp_pool[i].NO = i + 1;
-		wp_pool[i].next = &wp_pool[i + 1];
-	}
-	wp_pool[NR_WP - 1].next = NULL;
-
-	head = NULL;
-	free_ = wp_pool;
+  int i;
+  for(i = 0; i < NR_WP; i ++) {
+    wp_pool[i].NO = i + 1;
+    wp_pool[i].next = &wp_pool[i + 1];
+  }
+  wp_pool[NR_WP - 1].next = NULL;
+  
+  head = NULL;
+  free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
 
 WP* new_wp() {
-    if (free_==NULL) {
-        fputs("No more free watchpoint pool!\n", stderr);
-        return NULL;
+  if (free_==NULL) {
+    fputs("No more free watchpoint pool!\n", stderr);
+    return NULL;
+  }
+  else {
+    WP* p=free_;
+    free_=free_->next;
+    p->next=NULL;
+    if (head!=NULL) {
+      p->next=head;
     }
-    else {
-        WP* p=free_;
-        free_=free_->next;
-        p->next=NULL;
-        if (head!=NULL) {
-            p->next=head;
-        }
-        head=p;
-        return p;
-    }
+    head=p;
+    return p;
+  }
 }
 
 int free_wp(int NO) {
-        WP* p=head, *q=NULL;
-        if (NO!=head->NO) {
-            for (;p->next!=NULL;p=p->next) {
-                if (p->next->NO==NO) {
-                    break;
-                }
-            }
-            if (p->next==NULL) {
-                fputs("Cannot find the watchpoint!\n",stderr);
-                return 1;
-            }
-            else {
-	      q=p->next;
-                p->next=p->next->next;
-            }
-        }
-        else {
-            head=head->next;
-        }
-        for (p=free_;p->next!=NULL;p=p->next) {}
-        p->next=q;
-        q->next=NULL;
-        q->NO=p->NO+1;
-	return 0;
+  WP* p=head, *q=NULL;
+  if (NO!=head->NO) {
+    for (;p->next!=NULL;p=p->next) {
+      if (p->next->NO==NO) {
+	break;
+      }
+    }
+    if (p->next==NULL) {
+      fputs("Cannot find the watchpoint!\n",stderr);
+      return 1;
+    }
+    else {
+      q=p->next;
+      p->next=p->next->next;
+    }
+  }
+  else {
+    q=head;
+    head=head->next;
+  }
+  p=free_;
+  if (p!=NULL) {
+    for (p=free_;p->next!=NULL;p=p->next) {}
+    p->next=q;
+    q->next=NULL;
+    q->NO=p->NO+1;
+  }
+  else {
+    free_=q;
+    q->next=NULL;
+  }
+  return 0;
 }
