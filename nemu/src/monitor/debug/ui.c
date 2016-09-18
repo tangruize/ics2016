4361d845b7b76a8346be7dbde2ecaa55e37a15ae
tracer-ics2016 <tracer@njuics.org>
2016-09-18 13:51:43 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  13:51:43 up 55 min,  1 user,  load average: 0.02, 0.09, 0.25 d829552a6c1c1e572b9ae92df9f4960293265ac2

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -312,8 +312,11 @@ void ui_mainloop() {
     }
     if(i == NR_CMD) {
       bool success=true;
+       int len=strlen(cmd);
+        strcat(str, cmd+len);
       expr(str,&success,0);
       if (success==false) {
+          //cmd[len]='\0';
 	printf("Unknown command '%s'\n", cmd); 
       }
     }
