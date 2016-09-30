93baee43250e4ea6ecbe5ce193a640d57ce80562
tracer-ics2016 <tracer@njuics.org>
2016-09-30 18:15:10 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  18:15:10 up 22:40,  2 users,  load average: 0.22, 0.29, 0.31 a082f3bf12a0a40a75055a76fcaa1f2146df2018

---
+++ nemu/src/cpu/exec/call/call-template.h
@@ -0,0 +1,25 @@
+#include "cpu/exec/template-start.h"
+#include "cpu/reg.h"
+
+#define instr call
+
+static void do_execute() {
+  REG(R_ESP) -= DATA_BYTE;
+  OPERAND_W(op_dest, cpu.eip);
+  cpu.eip = (cpu.eip + op_src->val) & ((DATA_BYTE==2) ? 0x0000FFFF : 0xFFFFFFFF);
+  print_asm_template1();
+}
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_helper(concat(call_rel_, SUFFIX)) {
+  concat(decode_i_, SUFFIX)(eip);
+  op_dest->type = OP_TYPE_MEM;
+  op_dest->addr = REG(R_ESP);
+  op_dest->size = DATA_BYTE;
+  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
+  do_execute();
+  return 1 + DATA_BYTE;
+}
+#endif
+
+#include "cpu/exec/template-end.h"
