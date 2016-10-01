470d25faf2277e8ef3d5680d88668b7fcc17c84f
tracer-ics2016 <tracer@njuics.org>
2016-10-02 07:06:19 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  07:06:19 up 20:10,  2 users,  load average: 0.22, 0.16, 0.20 6b9d6a397870674c3d19852a6082da27f00ddb3

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -6,6 +6,6 @@ static void do_execute() {
   op_dest->type = OP_TYPE_MEM;
   op_dest->addr = REG(R_ESP);
   op_dest->size = DATA_BYTE;
-  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(op_dest->reg));
+  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
   OPERAND_W(op_src, op_dest->val);
 }
--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -7,7 +7,7 @@ static void do_execute() {
   op_dest->type = OP_TYPE_MEM;
   op_dest->addr = REG(R_ESP);
   op_dest->size = DATA_BYTE;
-  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(op_dest->reg));
+  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
   OPERAND_W(op_dest, op_src->val);
   print_asm_template1();
   //print_asm(str(instr) " %s", op_src->str);
