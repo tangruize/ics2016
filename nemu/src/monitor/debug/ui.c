9e0ec07035eb438897fe7a8506a908bff5d8f7e8
tracer-ics2016 <tracer@njuics.org>
2016-10-01 09:07:33 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  09:07:33 up 50 min,  1 user,  load average: 0.26, 0.30, 0.31 48731ba5d708ef3860b84766605c14a5699f60c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -12,18 +12,18 @@ void cpu_exec(uint32_t);
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 char* rl_gets() {
   static char *line_read = NULL;
-  
+
   if (line_read) {
     free(line_read);
     line_read = NULL;
   }
-  
+
   line_read = readline("(nemu) ");
-  
+
   if (line_read && *line_read) {
     add_history(line_read);
   }
-  
+
   return line_read;
 }
 
@@ -52,6 +52,8 @@ static int cmd_p(char *args);
 
 static int cmd_b(char *args);
 
+static int cmd_assign(char *args);
+
 static struct {
   char *name;
   char *description;
@@ -67,9 +69,10 @@ static struct {
   { "d", "Delete some breakpoints or auto-display expressions", cmd_d},
   { "p", "Print value of expression EXP.", cmd_p},
   { "b", "Set breakpoint at specified line", cmd_b},
+  { "assign", "Assign a specific register or memory address", cmd_assign},
 
   /* TODO: Add more commands */
-  
+
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
@@ -90,7 +93,7 @@ static int cmd_help(char *args) {
   /* extract the first argument */
   char *arg = strtok(NULL, " ");
   int i;
-  
+
   if(arg == NULL) {
     /* no argument given */
     for(i = 0; i < NR_CMD; i ++) {
@@ -296,6 +299,57 @@ static int cmd_b(char *args) {
   return cmd_w(str);
 }
 
+static int cmd_assign(char *args) {
+  char *arg1 = strtok(NULL, "=");
+  char *arg2 = NULL;
+  if (arg1!=NULL) {
+    arg2 = arg1+1+strlen(arg1);
+  }
+  if (arg2==NULL) {
+    fprintf(stderr, "%s\n", "Bad expression!");
+    return 0;
+  }
+  bool is_success=true;
+  uint32_t result2=(uint32_t)express(arg2, &is_success);
+  if (is_success) {
+    make_token(arg1, &is_success, 1);
+    if (nr_token==3) {
+      if (tokens[1].type==14) {
+        tokens[0].value+=tokens[2].value;
+      }
+      else if (tokens[1].type==15) {
+        tokens[0].value-=tokens[2].value;
+      }
+      else {
+        fprintf(stderr, "%s\n", "Bad expression!");
+        return 0;
+      }
+      swaddr_write((swaddr_t)tokens[0].value, 4, result2);
+    }
+    else if (nr_token==1) {
+      if (tokens[0].type==260) {
+        switch (tokens[0].str[2]) {
+          case 'a':cpu.eax=result2;break;
+          case 'c':cpu.ecx=result2;break;
+          case 'd':cpu.gpr[tokens[0].str[3]=='x'?2:7]._32=result2;break;
+          case 'b':cpu.gpr[tokens[0].str[3]=='x'?3:5]._32=result2;break;
+          case 's':cpu.gpr[tokens[0].str[3]=='p'?4:6]._32=result2;break;
+          case 'i':cpu.eip=result2;break;
+          default :assert(0);
+        }
+      }
+      else if (tokens[0].type==257 ||tokens[0].type== 258) {
+          swaddr_write((swaddr_t)tokens[0].value, 4, result2);
+      }
+    }
+  }
+  else {
+    fprintf(stderr, "%s\n", "Bad expression!");
+    return 0;
+  }
+  return 0;
+}
+
 void ui_mainloop() {
   while(1) {
     char *str = rl_gets();
@@ -308,7 +362,7 @@ void ui_mainloop() {
     /* extract the first token as the command */
     char *cmd = strtok(str, " ");
     if(cmd == NULL) { continue; }
-    
+
     /* treat the remaining string as the arguments,
      * which may need further parsing
      */
@@ -316,12 +370,12 @@ void ui_mainloop() {
     if(args >= str_end) {
       args = NULL;
     }
-    
+
     #ifdef HAS_DEVICE
     extern void sdl_clear_event_queue(void);
     sdl_clear_event_queue();
     #endif
-    
+
     int i;
     for(i = 0; i < NR_CMD; i ++) {
       if(strcmp(cmd, cmd_table[i].name) == 0) {
@@ -342,7 +396,7 @@ void ui_mainloop() {
           if (len!=0) {
             cmd[len]='\0';
           }
-	printf("Unknown command '%s'\n", cmd); 
+	printf("Unknown command '%s'\n", cmd);
       }
     }
   }
