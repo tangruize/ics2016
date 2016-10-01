05d44835acce20c1ce27c8ce6ac47a3a9d0537d2
tracer-ics2016 <tracer@njuics.org>
2016-10-02 07:43:38 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  07:43:38 up 20:47,  2 users,  load average: 0.31, 0.32, 0.30 7106c0ea6af75e28b8df292cc5e3f45c1904f10f

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -7,5 +7,21 @@ static void do_execute() {
   op_dest->addr = REG(R_ESP);
   op_dest->size = DATA_BYTE;
   snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
-  OPERAND_W(op_src, op_dest->val);
+  OPERAND_W(op_src, (uint32_t)swaddr_read(op_dest->addr, DATA_BYTE));
+  REG(R_ESP) += ((DATA_BYTE == 2) ? 2 : 4);
+  print_asm_template1();
 }
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(r)
+
+make_helper(concat(pop_m_, SUFFIX)) {
+  op_src->type = OP_TYPE_MEM;
+  op_src->addr = (uint32_t)swaddr_read(cpu.eip + 1, DATA_BYTE);
+  op_src->size = DATA_BYTE;
+  print_asm("pop" str(SUFFIX) " 0x%x", (uint32_t)op_src->addr);
+  return DATA_BYTE + 1;
+}
+#endif
+
+#include "cpu/exec/template-end.h"
