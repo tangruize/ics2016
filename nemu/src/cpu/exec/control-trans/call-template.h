235a408836bfb011539e663613e07b47598c4561
tracer-ics2016 <tracer@njuics.org>
2016-10-02 07:16:26 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  07:16:26 up 20:20,  2 users,  load average: 0.22, 0.14, 0.16 5c3341eba7112d1f168d2c57e8544fd6e6c6d306

--- nemu/src/cpu/exec/control-trans/call-template.h
+++ nemu/src/cpu/exec/control-trans/call-template.h
@@ -4,6 +4,10 @@
 
 static void do_execute() {
   REG(R_ESP) -= ((DATA_BYTE == 2) ? 2 : 4);
+  op_dest->type = OP_TYPE_MEM;
+  op_dest->addr = REG(R_ESP);
+  op_dest->size = DATA_BYTE;
+  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
   OPERAND_W(op_dest, cpu.eip);
   cpu.eip = (cpu.eip + op_src->val) & ((DATA_BYTE==2) ? 0x0000FFFF : 0xFFFFFFFF);
   print_asm_template1();
@@ -13,10 +17,6 @@ static void do_execute() {
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_helper(concat(call_rel_, SUFFIX)) {
   concat(decode_i_, SUFFIX)(eip + 1);
-  op_dest->type = OP_TYPE_MEM;
-  op_dest->addr = REG(R_ESP);
-  op_dest->size = DATA_BYTE;
-  snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
   do_execute();
   return 1 + DATA_BYTE;
 }
