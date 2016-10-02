5236d14d73f8bdc649fefa186ec74cc464bfab47
tracer-ics2016 <tracer@njuics.org>
2016-10-02 08:49:26 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  08:49:26 up 21:53,  2 users,  load average: 0.33, 0.31, 0.26 6c3b6cdd47326263352162babaabdb491ac044b9

--- nemu/src/cpu/exec/control-trans/ret-template.h
+++ nemu/src/cpu/exec/control-trans/ret-template.h
@@ -7,9 +7,14 @@
 static bool has_i = false;
 #endif
 
+#ifndef __AFTER_LEN__
+#define __AFTER_LEN__
+static int after_len = 0;
+#endif
+
 static void do_execute() {
   uint32_t tmp = (uint32_t)swaddr_read(REG(R_ESP) + (has_i ? op_src->val : 0), DATA_BYTE);
-  cpu.eip = tmp & ((DATA_BYTE==2) ? 0x0000FFFF : 0xFFFFFFFF);
+  cpu.eip = (tmp - after_len) & ((DATA_BYTE==2) ? 0x0000FFFF : 0xFFFFFFFF);
   if (has_i) {
     print_asm_template1();
   }
@@ -21,8 +26,9 @@ static void do_execute() {
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
 make_helper(concat(ret_, SUFFIX)) {
+  after_len = 1;
   do_execute();
-  return 0;
+  return 1;
 }
 
 make_helper(concat(ret_i_, SUFFIX)) {
@@ -31,8 +37,9 @@ make_helper(concat(ret_i_, SUFFIX)) {
   op_src->imm = (uint32_t)swaddr_read(cpu.eip + 1, 2);
   op_src->val = op_src->imm;
   snprintf(op_src->str, OP_STR_SIZE, "$0x%x", op_src->imm);
+  after_len = 3;
   do_execute();
-  return 0;
+  return 3;
 }
 #endif
 
