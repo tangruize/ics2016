b08ecde6e96e2a37bfca49a0948ed6697b32c100
tracer-ics2016 <tracer@njuics.org>
2016-10-06 09:28:28 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  09:28:28 up 22:35,  4 users,  load average: 0.65, 0.37, 0.34 484c52794285894515da3c442450a4c2872c0550

--- nemu/src/cpu/exec/arith/subb-template.h
+++ nemu/src/cpu/exec/arith/subb-template.h
@@ -12,7 +12,12 @@ static void do_execute() {
   OPERAND_W(op_dest, result);
 //  printf("%d\t%d\t%d\n",  op_src->val, op_dest->val, result);
 
-  eflags(OF) = set_of((int)op_dest->val, (int)-op_src->val, (DATA_BYTE==2?2:4));
+  if (op_src->val == 0x80000000) {
+      eflags(OF) = 1;
+  }
+  else {
+    eflags(OF) = set_of((int)op_dest->val, (int)-op_src->val, (DATA_BYTE==2?2:4));
+  }
   eflags(SF) = set_sf(result, (DATA_BYTE==2?2:4));
   eflags(ZF) = set_zf(result);
   eflags(PF) = set_pf(result);
