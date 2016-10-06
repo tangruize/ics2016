728b625c85d71f88c9af2e49b82a5a4702f8abc3
tracer-ics2016 <tracer@njuics.org>
2016-10-06 22:54:28 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  22:54:28 up 1 day, 12:01,  3 users,  load average: 2.53, 0.96, 0.55 926520feb8854f1a20c68dd12076159858f89a3d

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -56,7 +56,7 @@ make_helper(repnz) {
 			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
-		if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && eflags(ZF)==1) {
+		if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && eflags(ZF)==0) {
 			break;
 		}
 
