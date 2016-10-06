deba23fa8708d597aa1d4f5d9f54969cfe27cef3
tracer-ics2016 <tracer@njuics.org>
2016-10-06 14:11:09 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  14:11:09 up 1 day,  3:18,  4 users,  load average: 0.33, 0.24, 0.23 7bf5511868ed96f064a4cdc7ee66b964c46a80dc

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -4,8 +4,9 @@
 
 #if DATA_BYTE == 2 || DATA_BYTE ==4
 static void do_execute() {
-    OPERAND_W(op_dest, (DATA_TYPE_S)op_src->val);
-    print_asm_template2();
+  DATA_TYPE_S tmp = (DATA_TYPE_S)op_src->val;
+  OPERAND_W(op_dest, tmp);
+  print_asm_template2();
 }
 
 make_instr_helper(rmb2r)
