e7a51655fb0d98af0a7e09529e98f72ca0581020
tracer-ics2016 <tracer@njuics.org>
2016-10-07 14:21:37 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  14:21:37 up  6:10,  2 users,  load average: 0.24, 0.27, 0.24 75853aa3fbb33007e0e30d04522288e2e8a5e483

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -4,6 +4,7 @@
 FLOAT f(FLOAT x) {
 	/* f(x) = 1/(1+25x^2) */
 	FLOAT a = F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
+	set_bp();
 	if (x==0xffff0000 || x == 65536) {
 		nemu_assert(a == 2520);
 	}
