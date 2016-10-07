74cd6809b67bfae1cb82a36e1f0e6010a986331c
tracer-ics2016 <tracer@njuics.org>
2016-10-07 11:33:52 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  11:33:52 up  3:23,  2 users,  load average: 0.22, 0.32, 0.37 faa79db661cc486f54ecb1eb8c942c978aa29499

--- nemu/src/cpu/exec/control-trans/call-template.h
+++ nemu/src/cpu/exec/control-trans/call-template.h
@@ -30,6 +30,18 @@ make_helper(concat(call_i_, SUFFIX)) {
   do_execute();
   return 1 + DATA_BYTE;
 }
+
+make_helper(concat(cal_rm_, SUFFIX)) {
+  after_len = concat(decode_rm_, SUFFIX)(cpu.eip+1);
+  if (op_src->type==OP_TYPE_REG) {
+    snprintf(op_src->str, OP_STR_SIZE, "*%%%s", REG_NAME(op_src->reg));
+  }
+  cpu.eip = 0;
+  do_execute();
+  return after_len + 1;
+}
+
+
 #endif
 
 #include "cpu/exec/template-end.h"
