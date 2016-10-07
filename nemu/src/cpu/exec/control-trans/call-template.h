8caf5b487d50dee16aefecad1288544352ea7095
tracer-ics2016 <tracer@njuics.org>
2016-10-07 12:45:46 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  12:45:46 up  4:35,  2 users,  load average: 0.18, 0.25, 0.26 63239457b34077f220cfa34fc4e4ce102d9a6dc8

--- nemu/src/cpu/exec/control-trans/call-template.h
+++ nemu/src/cpu/exec/control-trans/call-template.h
@@ -8,6 +8,11 @@
 static int after_len = 0;
 #endif
 
+#ifndef __CALL_INDIRECT__
+#define __CALL_INDIRECT__
+static int call_indirect = 0;
+#endif
+
 static void do_execute() {
   #if DATA_BYTE == 2
   cpu.gpr[R_ESP]._16 -= 2;
@@ -19,8 +24,12 @@ static void do_execute() {
   op_dest->size = DATA_BYTE;
   snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_ESP));
   OPERAND_W(op_dest, cpu.eip + after_len + 1);
+  if (call_indirect) {
+    cpu.eip = -(after_len + 1);
+  }
   cpu.eip = (cpu.eip + op_src->val) & ((DATA_BYTE==2) ? 0x0000FFFF : 0xFFFFFFFF);
   print_asm_template1();
+  call_indirect=0;
   //print_asm(str(instr) " %s", op_src->str);
 }
 
@@ -37,7 +46,7 @@ make_helper(concat(call_rm_, SUFFIX)) {
   if (op_src->type==OP_TYPE_REG) {
     snprintf(op_src->str, OP_STR_SIZE, "*%%%s", REG_NAME(op_src->reg));
   }
-  cpu.eip = -(after_len + 1);
+  call_indirect = 1;
   do_execute();
   return after_len + 1;
 }
