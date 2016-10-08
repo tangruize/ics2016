39ba4a1bdaa9f84185efe583f92a3043bce2273b
tracer-ics2016 <tracer@njuics.org>
2016-10-08 13:00:04 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  13:00:04 up  5:29,  1 user,  load average: 0.16, 0.29, 0.34 42b026c7a296582aac587b43f225ee2717b81d0

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -62,14 +62,14 @@ void print_bin_instr(swaddr_t eip, int len) {
   l = sprintf(asm_buf, "%8x ", eip);
   set_in_func(eip);
   if (in_func.is_in) {
-    l += snprintf(asm_buf + l, 12, "<%s", all_elf_funcs[in_func.index].str);
+    l += snprintf(asm_buf + l, 8, "<%s", all_elf_funcs[in_func.index].str);
     l += snprintf(asm_buf + l, 12, "+0x%x>:   ", in_func.off);
   }
   else {
     l += sprintf(asm_buf + l, "<UNKNOWN>:   ");
   }
-  if (l!=31) {
-    l += sprintf(asm_buf + l, "%*s", 31-l, " ");
+  if (l!=27) {
+    l += sprintf(asm_buf + l, "%*s", 27-l, " ");
   }
   for(i = 0; i < len; i ++) {
     l += sprintf(asm_buf + l, "%02x ", instr_fetch(eip + i, 1));
