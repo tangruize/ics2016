61c3a93b566aaa8053eca9dfdb274978c780748a
tracer-ics2016 <tracer@njuics.org>
2016-10-08 19:33:13 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  19:33:13 up 12:02,  1 user,  load average: 0.83, 0.76, 0.68 14d9c01473baf8c4948e2c9599162137400276be

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -397,14 +397,14 @@ static int cmd_p(char *args) {
 	bool is_success=true;
 	//printf("%s\n", args);
 	bool p_hex=false;
-	if (args[0]=='\\' && args[1]=='x'){
+	if (args[0]=='%' && args[1]=='x'){
 		args+=2;
 		p_hex=true;
 	}
 	int result = express(args, &is_success);
 	if (is_success==true) {
 		if (p_hex) {
-			printf("%x\n", (unsigned)result);
+			printf("0x%x\n", (unsigned)result);
 		}
 		else {
 			printf("%d\n", result);
