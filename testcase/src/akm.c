41018b36cbad3daa921e76d8da58fda5af8379fb
tracer-ics2016 <tracer@njuics.org>
2016-10-09 21:32:54 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  21:32:55 up  8:51,  1 user,  load average: 0.80, 0.90, 0.87 e477796b99d2be08f2b90b3efb27f58550e577fa

--- testcase/src/akm.c
+++ testcase/src/akm.c
@@ -1,23 +1,13 @@
 #include "trap.h"
-
-int akm(int m, int n)
-{
+int akm(int m, int n) {
 	if (m == 0)
-	{
 		return n + 1;
-	}
 	else if (n == 0)
-	{
 		return akm(m - 1, 1);
-	}
 	else
-	{
 		return akm(m - 1, akm(m, n - 1));
-	}
 }
-
-int main()
-{
+int main() {
     int i=akm(3,7);
 	nemu_assert(i == 1021);
     return 0;
