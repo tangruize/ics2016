b2a5ae80e2e12b8c2151b90d4e55bbcfa9fb8926
tracer-ics2016 <tracer@njuics.org>
2016-10-09 20:21:36 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  20:21:36 up  7:39,  1 user,  load average: 0.57, 0.45, 0.57 facafe04c5c97cd164a6f3edfb490a5b868cd314

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -48,22 +48,6 @@ int set_in_func(swaddr_t eip){
 		in_func.is_in=false;
 		return 1;
 	}
-	uint8_t next_instr=(uint8_t)instr_fetch(eip, 1);
-	if (next_instr==0xf3) {
-		next_instr=(uint8_t)instr_fetch(eip+1, 1);
-	}
-	if (next_instr==0xc2 || next_instr==0xc3) {
-		is_return = true;
-		pre_eip=eip;
-		if (set_finish) {
-			if (call_cnt==0) {
-				nemu_state = STOP;
-				set_finish=false;
-				call_cnt=1;
-			}
-			--call_cnt;
-		}
-	}
 
 	if (in_func.is_in) {
 		if (eip >= all_elf_funcs[in_func.index].end || eip < all_elf_funcs[in_func.index].start) {
@@ -123,6 +107,24 @@ int set_in_func(swaddr_t eip){
 
 		is_return=false;
 		set_next_call=0;
+	}
+
+	uint8_t next_instr=(uint8_t)instr_fetch(eip, 1);
+
+	if (next_instr==0xf3) {
+		next_instr=(uint8_t)instr_fetch(eip+1, 1);
+	}
+	if (next_instr==0xc2 || next_instr==0xc3) {
+		is_return = true;
+		pre_eip=eip;
+		if (set_finish) {
+			if (call_cnt==0) {
+				nemu_state = STOP;
+				set_finish=false;
+				call_cnt=1;
+			}
+			--call_cnt;
+		}
 	}
 
 	if (next_instr==0xe8 || (next_instr == 0xff && (instr_fetch(eip+1, 1) & 0x30) == 0x10)) {
