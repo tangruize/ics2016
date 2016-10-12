d86d30c6e478fe9e6d0ab89f6ccc9fd971eda85c
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:33:46 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:33:46 up 1 day,  5:27,  2 users,  load average: 0.24, 0.28, 0.36 fc8ba98ddcc9c9dd61fcec80f8a1adaa7d98b803

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,31 +1,26 @@
 #include "trap.h"
 
-#define N 100
-
-struct dummy {
-	int pad1[N];
-	char pad2[N];
-} d;
-
-struct dummy fun(struct dummy a) {
-	return a;
+long long sub(long long a, long long b) {
+	long long c = a - b;
+	return c;
 }
 
+long long test_data[] = {0, 1, 2, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL};
+long long ans[] = {0LL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x8000000000000001LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x2LL, 0x1LL, 0x1LL, 0LL, 0xffffffffffffffffLL, 0x8000000000000002LL, 0x8000000000000001LL, 0x8000000000000000LL, 0x3LL, 0x2LL, 0x2LL, 0x1LL, 0LL, 0x8000000000000003LL, 0x8000000000000002LL, 0x8000000000000001LL, 0x4LL, 0x3LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x7ffffffffffffffdLL, 0LL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x8000000000000001LL, 0x8000000000000000LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x1LL, 0LL, 0xffffffffffffffffLL, 0x8000000000000002LL, 0x8000000000000001LL, 0x8000000000000001LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x2LL, 0x1LL, 0LL, 0x8000000000000003LL, 0x8000000000000002LL, 0xfffffffffffffffeLL, 0xfffffffffffffffdLL, 0xfffffffffffffffcLL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x7ffffffffffffffdLL, 0LL, 0xffffffffffffffffLL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0xfffffffffffffffdLL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x1LL, 0LL};
+
+#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
+
 int main() {
-	int i;
-	for(i = 0; i < N; i ++) {
-		d.pad1[i] = i + 128;
-		d.pad2[i] = i;
+	int i, j, ans_idx = 0;
+	int loop = 0;
+	for(i = 0; i < NR_DATA; i ++) {
+		for(j = 0; j < NR_DATA; j ++) {
+			nemu_assert(sub(test_data[i], test_data[j]) == ans[ans_idx ++]);
+			loop ++;
+		}
 	}
 
-	struct dummy t = fun(d);
-
-	for(i = 0; i < N; i ++) {
-		nemu_assert(t.pad1[i] == i + 128);
-		nemu_assert(t.pad2[i] == i);
-	}
-
-	nemu_assert(i == N);
+	nemu_assert(loop == NR_DATA * NR_DATA);
 
 	return 0;
 }
