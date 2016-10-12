5e641bb5a6588794ccc79364bf7a3bdf632ddf34
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:29:29 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:29:29 up 1 day,  5:23,  2 users,  load average: 0.35, 0.38, 0.41 1ff4f74dba4ecfba88d90a9873da6917c1f4f115

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,50 +1,20 @@
 #include "trap.h"
-#include "FLOAT.h"
 
-FLOAT f(FLOAT x) {
-	/* f(x) = 1/(1+25x^2) */
-	FLOAT a;
-	//set_bp();
-	a=F_mul_F(x, x);
-	if (x==65536)
-		nemu_assert(a == 65536);
-	a=F_mul_int(a, 25);
-	if (x==65536)
-		nemu_assert(a == 1638400);
-	a=int2F(1)+a;
-	if (x==65536)
-		nemu_assert(a == 1703936);
-	a=F_div_F(int2F(1), a);
-	if (x==65536)
-		nemu_assert(a == 2520);
-	//F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
-	return a;
+int is_leap_year(int n) {
+	return (n % 4 == 0 && n % 100 != 0) || (n % 400 == 0);
 }
 
-FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
-	nemu_assert(n == 10);
-	nemu_assert(a == 0xffff0000);
-	nemu_assert(b == 65536);
-	int k;
-	FLOAT s,h;
-	h = F_div_int((b - a), n);
-	nemu_assert(h == 13107);
-	s = F_div_int(fun(a) + fun(b), 2 );
-	nemu_assert(s == 2520);
-	for(k = 1; k < n; k ++) {
-		s += fun(a + F_mul_int(h, k));
-	}
-	nemu_assert(s == 180634);
-	s = F_mul_F(s, h);
-	nemu_assert(s == 36126);
-	return s;
-}
+int ans[] = {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0};
+
+#define NR_DATA (sizeof(ans) / sizeof(ans[0]))
 
 int main() {
-	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
-	FLOAT ans = f2F(0.551222);
+	int i;
+	for(i = 0; i < NR_DATA; i ++) {
+		nemu_assert(is_leap_year(i + 1890) == ans[i]);
+	}
 
-	nemu_assert(Fabs(a - ans) < f2F(1e-4));
+	nemu_assert(i == NR_DATA);
 
 	return 0;
 }
