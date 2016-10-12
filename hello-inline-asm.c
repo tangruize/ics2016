680b81f9fe38f7a57403407c5092b85b301718d9
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:31:24 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:31:24 up 1 day,  5:25,  2 users,  load average: 0.20, 0.31, 0.38 9e337d0c1d05dff793454bf3119c9985af77b4ee

--- hello-inline-asm.c
+++ hello-inline-asm.c
@@ -1,9 +1,31 @@
++ as testcase/src/start.S
++ cc testcase/src/tmp.c
++ ld obj/testcase/tmp
 objcopy -S -O binary obj/testcase/tmp entry
 obj/nemu/nemu obj/testcase/tmp
 Welcome to NEMU!
 The executable is obj/testcase/tmp.
 For help, type "help"
 (nemu) c
+invalid opcode(eip = 0x0010002c): cd 80 b8 00 00 00 00 5d ...
 
-[1;31mnemu: HIT GOOD TRAP[0m at eip = 0x00100014
-(nemu) q
+There are two cases which will trigger this unexpected exception:
+1. The instruction at eip = 0x0010002c is not implemented.
+2. Something is implemented incorrectly.
+Find this eip value(0x0010002c) in the disassembling result to distinguish which case it is.
+
+[1;31mIf it is the first case, see
+ _ ____   ___    __    __  __                         _ 
+(_)___ \ / _ \  / /   |  \/  |                       | |
+ _  __) | (_) |/ /_   | \  / | __ _ _ __  _   _  __ _| |
+| ||__ < > _ <| '_ \  | |\/| |/ _` | '_ \| | | |/ _` | |
+| |___) | (_) | (_) | | |  | | (_| | | | | |_| | (_| | |
+|_|____/ \___/ \___/  |_|  |_|\__,_|_| |_|\__,_|\__,_|_|
+
+for more details.
+
+If it is the second case, remember:
+* The machine is always right!
+* Every line of untested code is always wrong![0m
+
+Makefile:63: recipe for target 'run' failed
