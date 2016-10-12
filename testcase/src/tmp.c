e87596657f46f197b85838eaaf2be93542cfdff3
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:31:31 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:31:31 up 1 day,  5:25,  2 users,  load average: 0.32, 0.34, 0.39 ead0065f8c03867d55d31fe6435c0fd474d4db8

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,42 +1,27 @@
 #include "trap.h"
 
-#define N 100
+int ans[] = {153, 370, 371, 407};
 
-int a[N] = {81, 37, 64, 23, 38, 65, 56, 15, 8, 33, 85, 39, 71, 12, 77, 6, 82, 89, 80, 35, 0, 59, 73, 4, 61, 30, 74, 69, 13, 42, 68, 63, 9, 29, 47, 36, 99, 25, 21, 14, 60, 3, 2, 18, 26, 83, 53, 5, 43, 67, 88, 70, 76, 92, 94, 48, 34, 49, 66, 95, 78, 62, 32, 52, 16, 72, 27, 28, 22, 40, 84, 91, 96, 57, 87, 51, 98, 1, 10, 11, 24, 20, 19, 31, 7, 97, 50, 86, 79, 17, 75, 55, 93, 44, 58, 54, 45, 41, 90, 46};
-
-void select_sort() {
-	int i, j, k, t;
-	for(i = 0; i < N - 1; i ++) {
-		k = i;
-		for(j = i + 1; j < N; j ++) {
-			if(a[j] < a[k]) {
-				k = j;
-			}
-		}
-
-		t = a[i];
-		a[i] = a[k];
-		a[k] = t;
-	}
+int cube(int n) {
+	return n * n * n;
 }
 
 int main() {
-	select_sort();
+	int n, n2, n1, n0;
+	int k = 0;
+	for(n = 100; n < 1000; n ++) {
+		n2 = n / 100;
+		n1 = (n / 10) % 10;
+		n0 = n % 10;
 
-	int i;
-	for(i = 0; i < N; i ++) {
-		nemu_assert(a[i] == i);
+		if(n == cube(n2) + cube(n1) + cube(n0)) {
+			nemu_assert(n == ans[k]);
+			k ++;
+		}
 	}
 
-	nemu_assert(i == N);
-
-	select_sort();
-
-	for(i = 0; i < N; i ++) {
-		nemu_assert(a[i] == i);
-	}
-
-	nemu_assert(i == N);
+	nemu_assert(n == 1000);
+	nemu_assert(k == 4);
 
 	return 0;
 }
