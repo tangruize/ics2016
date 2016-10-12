25c787b8f21913d20a110a2ed47f92a988858293
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:31:29 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:31:29 up 1 day,  5:25,  2 users,  load average: 0.26, 0.33, 0.39 5b0f3ccd39240ddcdef88ae3dd89ef0855f82613

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,24 +1,49 @@
 #include "trap.h"
-#include "FLOAT.h"
 
-/* solve 0.5x^2 + 0.5x - 0.5 = 0 */
+#define N 100
+
+int a[N] = {81, 37, 64, 23, 38, 65, 56, 15, 8, 33, 85, 39, 71, 12, 77, 6, 82, 89, 80, 35, 0, 59, 73, 4, 61, 30, 74, 69, 13, 42, 68, 63, 9, 29, 47, 36, 99, 25, 21, 14, 60, 3, 2, 18, 26, 83, 53, 5, 43, 67, 88, 70, 76, 92, 94, 48, 34, 49, 66, 95, 78, 62, 32, 52, 16, 72, 27, 28, 22, 40, 84, 91, 96, 57, 87, 51, 98, 1, 10, 11, 24, 20, 19, 31, 7, 97, 50, 86, 79, 17, 75, 55, 93, 44, 58, 54, 45, 41, 90, 46};
+
+int partition(int *a, int p, int q) {
+	int pivot = a[p];
+	int i = p, j = q;
+	while(i < j) {
+		while(i < j && a[j] > pivot) j --;
+		a[i] = a[j];
+
+		while(i < j && a[i] <= pivot) i ++;
+		a[j] = a[i];
+	}
+
+	a[i] = pivot;
+	return i;
+}
+
+void quick_sort(int *a, int p, int q) {
+	if(p >= q) return;
+
+	int m = partition(a, p, q);
+	quick_sort(a, p, m - 1);
+	quick_sort(a, m + 1, q);
+}
 
 int main() {
-	FLOAT a = f2F(0.5);
-	FLOAT b = f2F(0.5);
-	FLOAT c = f2F(-0.5);
+	quick_sort(a, 0, N - 1);
 
-	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
-	FLOAT sqrt_dt = sqrt(dt);
+	int i;
+	for(i = 0; i < N; i ++) {
+		nemu_assert(a[i] == i);
+	}
 
-	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
-	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
+	nemu_assert(i == N);
 
-	FLOAT x1_ans = f2F(0.618);
-	FLOAT x2_ans = f2F(-1.618);
+	quick_sort(a, 0, N - 1);
 
-	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
-	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
+	for(i = 0; i < N; i ++) {
+		nemu_assert(a[i] == i);
+	}
+
+	nemu_assert(i == N);
 
 	return 0;
 }
