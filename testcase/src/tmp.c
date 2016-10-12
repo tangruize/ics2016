6f36f122cb06ac009574306e5482725de03db7b9
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:31:25 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:31:25 up 1 day,  5:25,  2 users,  load average: 0.20, 0.31, 0.38 c5084ad870af7e06705598ca6e074954abe03079

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,28 +1,50 @@
 #include "trap.h"
+#include "FLOAT.h"
 
-int if_else(int n) {
-	int cost;
-	if(n > 500) cost = 150;
-	else if(n > 300) cost = 100;
-	else if(n > 100) cost = 75;
-	else if(n > 50) cost = 50;
-	else cost = 0;
-
-	return cost;
+FLOAT f(FLOAT x) {
+	/* f(x) = 1/(1+25x^2) */
+	FLOAT a;
+	//set_bp();
+	a=F_mul_F(x, x);
+	if (x==65536)
+		nemu_assert(a == 65536);
+	a=F_mul_int(a, 25);
+	if (x==65536)
+		nemu_assert(a == 1638400);
+	a=int2F(1)+a;
+	if (x==65536)
+		nemu_assert(a == 1703936);
+	a=F_div_F(int2F(1), a);
+	if (x==65536)
+		nemu_assert(a == 2520);
+	//F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
+	return a;
 }
 
-int test_data[] = {-1, 0, 49, 50, 51, 99, 100, 101, 299, 300, 301, 499, 500, 501};
-int ans[] = {0, 0, 0, 0, 50, 50, 50, 75, 75, 75, 100, 100, 100, 150};
-
-#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
+FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
+	nemu_assert(n == 10);
+	nemu_assert(a == 0xffff0000);
+	nemu_assert(b == 65536);
+	int k;
+	FLOAT s,h;
+	h = F_div_int((b - a), n);
+	nemu_assert(h == 13107);
+	s = F_div_int(fun(a) + fun(b), 2 );
+	nemu_assert(s == 2520);
+	for(k = 1; k < n; k ++) {
+		s += fun(a + F_mul_int(h, k));
+	}
+	nemu_assert(s == 180634);
+	s = F_mul_F(s, h);
+	nemu_assert(s == 36126);
+	return s;
+}
 
 int main() {
-	int i, ans_idx = 0;
-	for(i = 0; i < NR_DATA; i ++) {
-		nemu_assert(if_else(test_data[i]) == ans[ans_idx ++]);
-	}
+	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
+	FLOAT ans = f2F(0.551222);
 
-	nemu_assert(i == NR_DATA);
+	nemu_assert(Fabs(a - ans) < f2F(1e-4));
 
 	return 0;
 }
