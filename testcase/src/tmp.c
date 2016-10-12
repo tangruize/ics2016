753df31e459389f57d1694095b5668c2207de23b
tracer-ics2016 <tracer@njuics.org>
2016-10-12 15:33:36 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  15:33:36 up 1 day,  5:27,  2 users,  load average: 0.11, 0.26, 0.36 533bd86e40e16a4f59f2dc91d881e11855ed07fc

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,18 +1,36 @@
 #include "trap.h"
 
-#define N 40
+int is_prime(int n) {
+	if(n < 2) return 0;
 
-int fib[N] = {1, 1};
-int ans[] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155};
-
-int main() {
 	int i;
-	for(i = 2; i < N; i ++) {
-		fib[i] = fib[i - 1] + fib[i - 2];
-		nemu_assert(fib[i] == ans[i]);
+	for(i = 2; i < n; i ++) {
+		if(n % i == 0) {
+			return 0;
+		}
 	}
 
-	nemu_assert(i == N);
+	return 1;
+}
+
+int gotbaha(int n) {
+	int i;
+	for(i = 2; i < n; i ++) {
+		if(is_prime(i) && is_prime(n - i)) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+int main() {
+	int n;
+	for(n = 4; n <= 100; n += 2) {
+		nemu_assert(gotbaha(n) == 1);
+	}
+
+	nemu_assert(n == 102);
 
 	return 0;
 }
