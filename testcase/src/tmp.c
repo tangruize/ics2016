e25c3d52e4fc10249541af1d360dee55cf2ca6c6
tracer-ics2016 <tracer@njuics.org>
2016-10-15 00:53:01 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  00:53:01 up  1:13,  3 users,  load average: 0.13, 0.29, 0.30 8886f96b5f6f635429b0032c5dcc482612f932b9

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,25 +1,26 @@
 #include "trap.h"
 
-int ans[] = {6, 28, 496};
+long long add(long long a, long long b) {
+	long long c = a + b;
+	return c;
+}
+
+long long test_data[] = {0, 1, 2, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL};
+long long ans[] = {0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0x1LL, 0x2LL, 0x3LL, 0x8000000000000000LL, 0x8000000000000001LL, 0x8000000000000002LL, 0xffffffffffffffffLL, 0LL, 0x2LL, 0x3LL, 0x4LL, 0x8000000000000001LL, 0x8000000000000002LL, 0x8000000000000003LL, 0LL, 0x1LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0LL, 0x7ffffffffffffffdLL, 0x7ffffffffffffffeLL, 0x8000000000000000LL, 0x8000000000000001LL, 0x8000000000000002LL, 0xffffffffffffffffLL, 0LL, 0x1LL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0x8000000000000001LL, 0x8000000000000002LL, 0x8000000000000003LL, 0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0LL, 0x7ffffffffffffffdLL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0xfffffffffffffffcLL, 0xfffffffffffffffdLL, 0xffffffffffffffffLL, 0LL, 0x1LL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0xfffffffffffffffdLL, 0xfffffffffffffffeLL};
+
+#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-	int n, sum, i, k = 0;
-	for(n = 1; n < 500; n ++) {
-		sum = 0;
-		for(i = 1; i < n; i ++) {
-			if(n % i == 0) {
-				sum += i;
-			}
-		}
-		
-		if(sum == n) {
-			nemu_assert(n == ans[k]);
-			k ++;
+	int i, j, ans_idx = 0;
+	int loop = 0;
+	for(i = 0; i < NR_DATA; i ++) {
+		for(j = 0; j < NR_DATA; j ++) {
+			nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
+			loop ++;
 		}
 	}
 
-	nemu_assert(n == 500);
-	nemu_assert(k == 3);
+	nemu_assert(loop == NR_DATA * NR_DATA);
 
 	return 0;
 }
