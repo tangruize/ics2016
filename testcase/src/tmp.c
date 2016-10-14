fc77fd5c96e0d940df582dc505607070463909ba
tracer-ics2016 <tracer@njuics.org>
2016-10-15 00:53:02 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  00:53:02 up  1:13,  3 users,  load average: 0.13, 0.29, 0.30 39e8be50dfcb9c6fdec7e57bd00ee4d32b27b48d

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,14 +1,46 @@
 #include "trap.h"
-int akm(int m, int n) {
-	if (m == 0)
-		return n + 1;
-	else if (n == 0)
-		return akm(m - 1, 1);
-	else
-		return akm(m - 1, akm(m, n - 1));
+
+typedef unsigned char uint8_t;
+typedef char bool;
+
+bool getbit(void *buf, int offset){
+	int byte = offset >> 3;
+	offset &= 7;
+	uint8_t mask = 1 << offset;
+	return (((uint8_t *)buf)[byte] & mask) != 0;
 }
+
+void setbit(void *buf, int offset, bool bit){
+	int byte = offset >> 3;
+	offset &= 7;
+	uint8_t mask = 1 << offset;
+	
+	uint8_t * volatile p = buf + byte;
+	*p = (bit == 0 ? (*p & ~mask) : (*p | mask));
+}
+
 int main() {
-    int i=akm(2,4);
-	nemu_assert(i == 11);
-    return 0;
+	uint8_t buf[2];
+
+	buf[0] = 0xaa; 
+	nemu_assert(getbit(buf, 0) == 0);
+	nemu_assert(getbit(buf, 1) == 1);
+	nemu_assert(getbit(buf, 2) == 0);
+	nemu_assert(getbit(buf, 3) == 1);
+	nemu_assert(getbit(buf, 4) == 0);
+	nemu_assert(getbit(buf, 5) == 1);
+	nemu_assert(getbit(buf, 6) == 0);
+	nemu_assert(getbit(buf, 7) == 1);
+
+	setbit(buf, 8, 1);
+	setbit(buf, 9, 0);
+	setbit(buf, 10, 1);
+	setbit(buf, 11, 0);
+	setbit(buf, 12, 1);
+	setbit(buf, 13, 0);
+	setbit(buf, 14, 1);
+	setbit(buf, 15, 0);
+	nemu_assert(buf[1] == 0x55);
+
+	return 0;
 }
