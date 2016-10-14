d9ae6069ca29c90003a3b4e55e021d41c30ffeb7
tracer-ics2016 <tracer@njuics.org>
2016-10-15 00:53:02 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  00:53:02 up  1:13,  3 users,  load average: 0.13, 0.29, 0.30 f51d45ee2d25b977459bf4b7db05dbda1ba8620c

--- testcase/src/tmp.c
+++ testcase/src/tmp.c
@@ -1,26 +1,14 @@
 #include "trap.h"
-
-int add(int a, int b) {
-	int c = a + b;
-	return c;
+int akm(int m, int n) {
+	if (m == 0)
+		return n + 1;
+	else if (n == 0)
+		return akm(m - 1, 1);
+	else
+		return akm(m - 1, akm(m, n - 1));
 }
-
-int test_data[] = {0, 1, 2, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffffffff};
-int ans[] = {0, 0x1, 0x2, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffffffff, 0x1, 0x2, 0x3, 0x80000000, 0x80000001, 0x80000002, 0xffffffff, 0, 0x2, 0x3, 0x4, 0x80000001, 0x80000002, 0x80000003, 0, 0x1, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffffffff, 0, 0x7ffffffd, 0x7ffffffe, 0x80000000, 0x80000001, 0x80000002, 0xffffffff, 0, 0x1, 0x7ffffffe, 0x7fffffff, 0x80000001, 0x80000002, 0x80000003, 0, 0x1, 0x2, 0x7fffffff, 0x80000000, 0xfffffffe, 0xffffffff, 0, 0x7ffffffd, 0x7ffffffe, 0x7fffffff, 0xfffffffc, 0xfffffffd, 0xffffffff, 0, 0x1, 0x7ffffffe, 0x7fffffff, 0x80000000, 0xfffffffd, 0xfffffffe};
-
-#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
-
 int main() {
-	int i, j, ans_idx = 0;
-	int loop = 0;
-	for(i = 0; i < NR_DATA; i ++) {
-		for(j = 0; j < NR_DATA; j ++) {
-			nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
-			loop ++;
-		}
-	}
-
-	nemu_assert(loop == NR_DATA * NR_DATA);
-
-	return 0;
+    int i=akm(2,4);
+	nemu_assert(i == 11);
+    return 0;
 }
