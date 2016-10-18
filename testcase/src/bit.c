83d019b058c585716d0fcc112361e87351f23122
tracer-ics2016 <tracer@njuics.org>
2016-10-18 23:40:03 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  23:40:03 up 1 day, 16:03,  3 users,  load average: 1.01, 1.05, 1.20 fda0e920a9b9d352400328e7d93441ec2c6bea85

--- testcase/src/bit.c
+++ testcase/src/bit.c
@@ -23,7 +23,7 @@ int main() {
 	uint8_t buf[2];
 
 	buf[0] = 0xaa; 
-/*	nemu_assert(getbit(buf, 0) == 0);
+	nemu_assert(getbit(buf, 0) == 0);
 	nemu_assert(getbit(buf, 1) == 1);
 	nemu_assert(getbit(buf, 2) == 0);
 	nemu_assert(getbit(buf, 3) == 1);
@@ -31,10 +31,10 @@ int main() {
 	nemu_assert(getbit(buf, 5) == 1);
 	nemu_assert(getbit(buf, 6) == 0);
 	nemu_assert(getbit(buf, 7) == 1);
-*/
+
 	setbit(buf, 8, 1);
     nemu_assert(buf[1] == 0xf7);
-/*	setbit(buf, 9, 0);
+	setbit(buf, 9, 0);
 	setbit(buf, 10, 1);
 	setbit(buf, 11, 0);
 	setbit(buf, 12, 1);
@@ -42,6 +42,6 @@ int main() {
 	setbit(buf, 14, 1);
 	setbit(buf, 15, 0);
 	nemu_assert(buf[1] == 0x55);
-*/
+
 	return 0;
 }
