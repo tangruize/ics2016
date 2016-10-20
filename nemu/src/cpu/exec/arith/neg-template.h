d7c4b53b9798726e9c3f358ec6d5a68363455978
tracer-ics2016 <tracer@njuics.org>
2016-10-20 16:38:23 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  16:38:23 up  8:34,  4 users,  load average: 0.53, 0.27, 0.27 7796a52cfcdcc86cb3c7000f5ab81668543a536

--- nemu/src/cpu/exec/arith/neg-template.h
+++ nemu/src/cpu/exec/arith/neg-template.h
@@ -3,10 +3,16 @@
 #define instr neg
 
 static void do_execute() {
+	if (op_src->val==0) {
+		eflags(CF)=0;
+	}
+	else {
+		eflags(CF)=1;
+	}
 	DATA_TYPE result = -op_src->val;
 	OPERAND_W(op_src, result);
 
-	/* There is no need to update EFLAGS, since no other instructions 
+	/* There is no need to update EFLAGS, since no other instructions
 	 * in PA will test the flags updated by this instruction.
 	 */
 
