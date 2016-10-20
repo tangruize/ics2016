d89a06ec752116b0b6da9167dc54640f9ef4cf3c
tracer-ics2016 <tracer@njuics.org>
2016-10-20 14:28:05 +0800

    >  run 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  14:28:05 up  6:24,  3 users,  load average: 0.12, 0.18, 0.31 469036f4b28d8cf1469389fb9956ec1ba6e30f0

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -5,16 +5,18 @@
 
 int main() {
 	FLOAT f=0xfffecccd;
-	/*char buf[80];
+	char buf[80];
 	int round_num;
-	int decimal;*/
+	//int decimal;
 	int is_neg=(f<0);
 	f=is_neg?-f:f;
 	nemu_assert(f == 0x00013333);
-	/*
-	decimal=(((long long)f&0xffff)*1000000)>>16;
+	
+	//decimal=(((long long)f&0xffff)*1000000)>>16;
 	round_num =(f&0x7fffffff)>>16;
-	int len = sprintf(buf, "%d.", is_neg?-round_num:round_num);
+	/*int len = */sprintf(buf, "%d.", is_neg?-round_num:round_num);
+	nemu_assert(strcmp(buf, "-1.") == 0);
+	/*
 	int len2 = sprintf(buf+len, "%d", decimal);
 	while (len2<6) {
 		buf[len+len2]='0';
