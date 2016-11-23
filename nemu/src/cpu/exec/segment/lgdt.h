bcbf741a68dd2cc3cc9afe2d44fce18ea8a4b69b
tracer-ics2016 <tracer@njuics.org>
2016-11-23 23:51:52 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  23:51:52 up 1 day,  2:05,  2 users,  load average: 0.54, 0.41, 0.29 e8149154daaabe1b1ffe9b32e9b29d319365b794

--- nemu/src/cpu/exec/segment/lgdt.h
+++ nemu/src/cpu/exec/segment/lgdt.h
@@ -7,7 +7,7 @@ make_helper(lgdt) {
   decode_rm_l(eip+1);
   cpu.GDTR.limit=lnaddr_read(op_src->val, 2);
   cpu.GDTR.base=lnaddr_read(op_src->val+2, 4);
-  printf("gdtr: %x %x\n", cpu.GDTR.limit, cpu.GDTR.base);
+  printf("gdtr:%x %x %x\n", op_src->val, cpu.GDTR.limit, cpu.GDTR.base);
   return 6;
 }
 
