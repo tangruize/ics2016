d8ae9368ed8c6edc04a7cd2e7edb676d71f07b1d
tracer-ics2016 <tracer@njuics.org>
2016-11-23 22:04:39 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  22:04:39 up 1 day, 18 min,  2 users,  load average: 0.54, 0.38, 0.34 9bbf322869190823bf52a7ce5ff67a476e62ea2

--- nemu/src/cpu/exec/control-trans/jmp-template.h
+++ nemu/src/cpu/exec/control-trans/jmp-template.h
@@ -55,14 +55,14 @@ make_helper(jmp_ptr)  {
   }SD;
   SegDesc tmp;
   int x=swaddr_read(((int32_t)op_src->val+cpu.GDTR.base), 4, R_DS);
-  memcpy((void*)&tmp+4, (void*)&x, 4);
-  x=swaddr_read(((int32_t)op_src->val+cpu.GDTR.base)+4, 4, R_DS);
   memcpy((void*)&tmp, (void*)&x, 4);
+  x=swaddr_read(((int32_t)op_src->val+cpu.GDTR.base)+4, 4, R_DS);
+  memcpy((void*)&tmp+4, (void*)&x, 4);
   SD.Split.base_15_0=tmp.base_15_0;
   SD.Split.base_23_16=tmp.base_23_16;
   SD.Split.base_31_24=tmp.base_31_24;
   cpu.eip += SD.Merge.base;
-  printf("%x\n", SD.Merge.base);
+  printf("%x\n", cpu.GDTR.base);
   return 7;
 }
 #endif
