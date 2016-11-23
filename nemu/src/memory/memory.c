9c8277cd8c880d0ba97b0e34cce5615303a26a42
tracer-ics2016 <tracer@njuics.org>
2016-11-23 22:13:14 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  22:13:14 up 1 day, 27 min,  2 users,  load average: 0.41, 0.29, 0.31 c4b9638e36a87d8b7901168575dd9d0cdf2e50b1

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -30,7 +30,8 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 
 lnaddr_t seg_translate(swaddr_t addr, uint8_t sreg) {
 	if (cpu.CR0.PE) {
-		//return sreg_base(sreg)+addr;
+		printf("%x %x\n",sreg, sreg_base(sreg));
+		return sreg_base(sreg)+addr;
 	}
 	else {
 		return addr;
