cf34dfd6d79be1f9f73834bb1a47610a41ca6a7c
tracer-ics2016 <tracer@njuics.org>
2016-11-24 16:51:57 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  16:51:57 up 1 day, 19:05,  2 users,  load average: 0.33, 0.46, 0.57 16274adb6b709850ebfa7dfe7fd1c73f6ba823f5

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -27,7 +27,7 @@ hwaddr_t page_translate(lnaddr_t lnaddr) {
 		};
 		uint32_t val;
 	} addr;
-	if (cpu.CR0.protect_enable==0||cpu.CR0.paging==0) {
+	if (cpu.CR0.paging==0) {
 		if (lnaddr>0xc0000000) {
 			printf("%x %x %x\n",cpu.CR0.protect_enable, cpu.CR0.paging, lnaddr);
 		}
