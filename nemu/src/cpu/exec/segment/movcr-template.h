9a64bbcdc446f91dd9b7341eec14acc2b040b80d
tracer-ics2016 <tracer@njuics.org>
2016-11-24 19:15:40 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  19:15:40 up 42 min,  2 users,  load average: 0.14, 0.25, 0.30 c58bd4b17644f63fcc6aa4695da82bfb65ef0fc5

--- nemu/src/cpu/exec/segment/movcr-template.h
+++ nemu/src/cpu/exec/segment/movcr-template.h
@@ -5,6 +5,7 @@
 //22
 make_helper(movcr_r2rm) {
 	int len=decode_r2rm_l(eip+1);
+	printf("%x %x %x %x\n",op_dest->reg,op_dest->val,op_src->reg, creg(op_src->reg));
 	creg(op_src->reg)=op_dest->val;
 	return len+1;
 }
