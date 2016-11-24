b48b6521a3371b71c5cd9511c0832b190430a68e
tracer-ics2016 <tracer@njuics.org>
2016-11-24 16:47:56 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  16:47:56 up 1 day, 19:01,  2 users,  load average: 0.36, 0.53, 0.63 d0f4044fe2116fbcf4e5aab26f0e340dc8982456

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -33,6 +33,7 @@ hwaddr_t page_translate(lnaddr_t lnaddr) {
 	addr.val=lnaddr;
 	uint32_t pdb=cpu.CR3.page_directory_base;
 	#define MSB_20 0xfffff000
+	printf("%x\n", pdb);
 	uint32_t PDE_page_frame=hwaddr_read(pdb+(addr.dir<<2), 4)&MSB_20;
 	printf("%x\n", PDE_page_frame);
 	uint32_t PTE_page_frame=hwaddr_read(PDE_page_frame+(addr.page<<2),4)&MSB_20;
