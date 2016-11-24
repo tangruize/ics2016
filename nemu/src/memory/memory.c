dce3dc8c3d0e2ccec50f8f4e1572a4a50c5b7635
tracer-ics2016 <tracer@njuics.org>
2016-11-24 17:01:26 +0800

    >  compile NEMU 151220100 cs Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  17:01:26 up 1 day, 19:15,  2 users,  load average: 0.14, 0.26, 0.45 eaebbd66d79bd0678b3560ff75bf37808572c78e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,18 +28,20 @@ hwaddr_t page_translate(lnaddr_t lnaddr) {
 		uint32_t val;
 	} addr;
 	if (cpu.CR0.paging==0) {
-		if (lnaddr>0xc0000000) {
+		/*if (lnaddr>0xc0000000) {
 			printf("%x %x %x\n",cpu.CR0.protect_enable, cpu.CR0.paging, lnaddr);
-		}
+		}*/
 		return lnaddr;
 	}
 	addr.val=lnaddr;
 	uint32_t pdb=cpu.CR3.page_directory_base;
-	fprintf(stderr,"%x\n", pdb);
-	uint32_t PDE_page_frame=hwaddr_read(pdb+(addr.dir<<2), 4);
-	printf("%x\n", PDE_page_frame);
-	uint32_t PTE_page_frame=hwaddr_read(PDE_page_frame+(addr.page<<2),4);
-	printf("%x %x %x %x\n",pdb, PDE_page_frame, PTE_page_frame, PTE_page_frame+addr.off);
+	//fprintf(stderr,"%x\n", pdb);
+	uint32_t PDE_page_frame=hwaddr_read((pdb<<12)+(addr.dir<<2), 4);
+	assert(PDE_page_frame&0x1);
+	//printf("%x\n", PDE_page_frame);
+	uint32_t PTE_page_frame=hwaddr_read((PDE_page_frame&0xfffff000)+(addr.page<<2),4);
+	assert(PTE_page_frame&0x1);
+	//printf("%x %x %x %x\n",pdb, PDE_page_frame, PTE_page_frame, PTE_page_frame+addr.off);
 	return PTE_page_frame+addr.off;
 }
 
