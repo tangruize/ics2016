9964e874f34de3de0241dcd7a370c5353208f1aa
tracer-ics2016 <tracer@njuics.org>
2016-12-14 19:41:54 +0800

    >  compile kernel 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  19:41:54 up 3 days, 10:04,  2 users,  load average: 0.11, 0.12, 0.22 82d8d8025a45347557c508d0ac3f1d0f299977b1

--- kernel/include/irq.h
+++ kernel/include/irq.h
@@ -10,12 +10,12 @@
 
 typedef struct TrapFrame {
 	//uint32_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, old_esp, edi, ebp;
-	/*uint32_t eflags, cs, eip, error_code;
+	uint32_t eflags, cs, eip, error_code;
 	int32_t irq;
-	uint32_t eax, ecx, edx, ebx, old_esp, ebp, esi, edi;*/
-	uint32_t edi, ebp, old_esp, ebx, edx, ecx, eax;
+	uint32_t eax, ecx, edx, ebx, old_esp, ebp, esi, edi;
+	/*uint32_t edi, ebp, old_esp, ebx, edx, ecx, eax;
 	int32_t irq;
-	uint32_t error_code, eip, cs, eflags;
+	uint32_t error_code, eip, cs, eflags;*/
 } TrapFrame;
 
 #endif
