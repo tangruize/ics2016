93d526302ea69f293c5c828f9e8e40eebc58ea1c
tracer-ics2016 <tracer@njuics.org>
2016-12-14 23:06:39 +0800

    >  compile kernel 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  23:06:39 up 3 days, 13:29,  2 users,  load average: 0.50, 0.73, 0.82 c1f5cce5d8f9b56d686fbf3ad8a1cfc4c86040

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -37,6 +37,7 @@ void do_syscall(TrapFrame *tf) {
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_ioctl: sys_ioctl(tf); break;
 		case 4:
+			panic("hhh");
 			tf->eax = sys_write(tf->ebx, (void*)tf->ecx, tf->edx);
 			break;
 
