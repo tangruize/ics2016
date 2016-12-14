b055ea22c27d3a67a8257431f88eb90d5a69c1bf
tracer-ics2016 <tracer@njuics.org>
2016-12-14 23:43:28 +0800

    >  compile NEMU 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  23:43:28 up 3 days, 14:06,  2 users,  load average: 1.04, 0.80, 0.67 c09e69e4eb6e06237999aba580ccdc0f9b6f8fa

--- nemu/src/cpu/exec/special/special.c
+++ nemu/src/cpu/exec/special/special.c
@@ -33,7 +33,7 @@ make_helper(nemu_trap) {
 		{
 				void *ad = (void *)seg_translate((swaddr_t)cpu.ecx, R_DS);
 				ad = (void *)page_translate((lnaddr_t)ad);
-				printf("%d\n", cpu.edx);
+				printf("%d\n",cpu.ebx);
 				write(cpu.ebx, ad, cpu.edx);
 		   	break;
 		}
