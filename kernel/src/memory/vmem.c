a2570b4b33a54cfbdcfe0a8611764705558d2b31
tracer-ics2016 <tracer@njuics.org>
2016-12-15 21:48:04 +0800

    >  compile kernel 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  21:48:04 up 36 min,  2 users,  load average: 0.16, 0.29, 0.36 8fd71764fa88177ebb027216c91ab72743373e85

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -8,7 +8,7 @@
 /* Use the function to get the start address of user page directory. */
 PDE* get_updir();
 
-static PTE kptable[NR_PTE] align_to_page;		// kernel page tables
+static PTE kptable[NR_PTE] align_to_page;
 void create_video_mapping() {
 	/* TODO: create an identical mapping from virtual memory area
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area
@@ -19,13 +19,11 @@ void create_video_mapping() {
 	uint32_t pdir_idx = 0;
 	PDE *pdir = (PDE *)va_to_pa(get_updir());
 	PTE *ptable = (PTE *)va_to_pa(kptable+0xa0);
-	ptable += NR_PTE;
- 	pdir[pdir_idx].val = make_pde(ptable);
-	int32_t pframe_addr = VMEM_ADDR - PAGE_SIZE;
-	ptable --;
-	for (; pframe_addr >= 0; pframe_addr -= PAGE_SIZE) {
+ 	pdir[pdir_idx].val = make_pde(kptable);
+	int32_t pframe_addr = VMEM_ADDR;
+	for (; pframe_addr < VMEM_ADDR + SCR_SIZE; pframe_addr += PAGE_SIZE) {
 		ptable->val = make_pte(pframe_addr);
-		ptable --;
+		ptable ++;
 	}
 	//set_bp();
 	//panic("please implement me");
