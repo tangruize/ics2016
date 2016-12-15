d6649b5ecd3922370d46e3d1c081c29ae9ed49f4
tracer-ics2016 <tracer@njuics.org>
2016-12-15 14:46:36 +0800

    >  compile kernel 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  14:46:36 up 4 days,  5:09,  1 user,  load average: 0.29, 0.36, 0.35 827a2ced0827abbcf23f5df1d894cf429d87628

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -9,12 +9,26 @@
 PDE* get_updir();
 
 void create_video_mapping() {
-	/* TODO: create an identical mapping from virtual memory area 
-	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area 
+	/* TODO: create an identical mapping from virtual memory area
+	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memory area
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	panic("please implement me");
+	uint32_t pdir_idx;
+	PDE *pdir = get_updir();
+	PTE *ptable = (PTE *)pdir + 4096;
+ 	for (pdir_idx = 0; pdir_idx < SCR_SIZE / PT_SIZE; pdir_idx ++) {
+ 		pdir[pdir_idx].val = make_pde(ptable);
+ 		ptable += NR_PTE;
+ 	}
+	asm volatile ("std;\
+	 1: stosl;\
+		subl %0, %%eax;\
+		jge 1b;\
+		cld" : :
+		"i"(PAGE_SIZE), "a"((VMEM_ADDR + SCR_SIZE - PAGE_SIZE) | 0x7), "D"(ptable - 1));
+
+	//panic("please implement me");
 }
 
 void video_mapping_write_test() {
@@ -36,4 +50,3 @@ void video_mapping_read_test() {
 void video_mapping_clear() {
 	memset((void *)VMEM_ADDR, 0, SCR_SIZE);
 }
-
