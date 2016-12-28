97b949f400b8846bcb0e91859a10edeb31533bf4
tracer-ics2016 <tracer@njuics.org>
2016-12-28 23:05:36 +0800

    >  compile NEMU 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  23:05:36 up 1 day,  3:15,  2 users,  load average: 0.49, 1.00, 2.73 23027cd76d7bdc831c96645861b2d6703a391d6b

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -7,6 +7,11 @@ typedef struct {
 	uint32_t disk_offset;
 } file_info;
 
+typedef	struct	{
+	 bool	opened;
+	 uint32_t	offset;
+}	Fstate;
+
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 
 /* This is the information about all files in disk. */
@@ -26,6 +31,16 @@ static const file_info file_table[] __attribute__((used)) = {
 	{"word.dat", 5650, 28155717},
 };
 
+static const Fstate file_state[] __attribute__((used)) = {
+	{false, 0}, {false, 0}, {false, 0}, {false, 0},
+	{false, 0}, {false, 0}, {false, 0}, {false, 0},
+	{false, 0}, {false, 0}, {false, 0}, {false, 0},
+	{false, 0}, {false, 0}, {false, 0}, {false, 0},
+	{false, 0}, {false, 0}, {false, 0}, {false, 0},
+	{false, 0}, {false, 0}, {false, 0}, {false, 0},
+	{false, 0},
+};
+
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
 int fs_ioctl(int fd, uint32_t request, void *p) {
@@ -37,4 +52,44 @@ void ide_read(uint8_t *, uint32_t, uint32_t);
 void ide_write(uint8_t *, uint32_t, uint32_t);
 
 /* TODO: implement a simplified file system here. */
+int	fs_open(const	char	*pathname,	int	flags) {	/*	在我们的实现中可以忽略flags	*/
+	int i = 0;
+	for (; i<NR_FILES; ++i) {
+		if (strcmp(pathname, file_table[i].name) == 0) {
+			file_state[i].opened = true;
+			return i+3;
+		}
+	}
+	panic("Invalid pathname: %s", pathname);
+	return -1;
+}
+int	fs_read(int	fd,	void	*buf,	int	len) {
+	ide_read(buf, file_table[fd - 3].disk_offset + file_state[fd - 3].offset, len);
+	file_state[fd - 3].offset += len;
+	if (file_state[fd - 3].offset >= file_table[fd - 3].size) {
+		len -= file_state[fd - 3].offset - file_table[fd - 3].size;
+		file_state[fd - 3].offset = file_table[fd - 3].size;
+	}
+	return len;
+}
+int	fs_write(int	fd,	void	*buf,	int	len) {
+	ide_write(buf, file_table[fd - 3].disk_offset + file_state[fd - 3].offset, len);
+	file_state[fd - 3].offset += len;
+	return len;
+}
 
+int	fs_lseek(int	fd,	int	offset,	int	whence) {
+	switch (whence) {
+		case SEEK_SET: file_state[fd - 3].offset = offset; break;
+		case SEEK_CUR: file_state[fd - 3].offset += offset; break;
+		case SEEK_END: file_state[fd - 3].offset = file_table[fd - 3].size + offset; break;
+		default : panic("No such whence type.");
+	}
+	return file_state[fd - 3].offset;
+}
+
+int	fs_close(int	fd) {
+	file_state[fd - 3].opened = false;
+	file_state[fd - 3].offset = 0;
+	return 0;
+}
--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -15,6 +15,7 @@ static void sys_ioctl(TrapFrame *tf) {
 	tf->eax = fs_ioctl(tf->ebx, tf->ecx, (void *)tf->edx);
 }
 
+int	fs_write(int	fd,	void	*buf,	int	len);
 ssize_t sys_write(int fd, const void *buf, size_t len) {
 	#ifndef HAS_DEVICE
 	asm	volatile(".byte	0xd6"	:	:	"a"(2),	"b"(fd), "c"(buf),	"d"(len));
@@ -22,6 +23,9 @@ ssize_t sys_write(int fd, const void *buf, size_t len) {
 	asm volatile("movl %%eax, %0" : "=r"(val));
 	return val;
 	#else
+	if (fd > 2) {
+		return fs_write(fd,	buf, len);
+	}
 	int i =0;
 	const char *p = buf;
 	for (;i<len;++i) {
@@ -31,6 +35,10 @@ ssize_t sys_write(int fd, const void *buf, size_t len) {
 	#endif
 }
 
+int	fs_read(int	fd,	void	*buf,	int	len);
+int	fs_open(const	char	*pathname,	int	flags);
+int	fs_close(int	fd);
+int	fs_lseek(int	fd,	int	offset,	int	whence);
 void do_syscall(TrapFrame *tf) {
 	switch(tf->eax) {
 		/* The `add_irq_handle' system call is artificial. We use it to
@@ -46,9 +54,22 @@ void do_syscall(TrapFrame *tf) {
 
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_ioctl: sys_ioctl(tf); break;
+		case 3:
+			tf->eax = fs_read(tf->ebx, (void*)tf->ecx, tf->edx);
+			break;
 		case 4:
 			tf->eax = sys_write(tf->ebx, (void*)tf->ecx, tf->edx);
 			break;
+		case 5:
+			tf->eax = fs_open((void*)tf->ebx, tf->ecx);
+			break;
+		case 6:
+			tf->eax = fs_close(tf->ebx);
+			break;
+		case 19:
+			tf->eax = fs_lseek(tf->ebx, tf->ecx, tf->edx);
+			break;
+
 
 		/* TODO: Add more system calls. */
 
