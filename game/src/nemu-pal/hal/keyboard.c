c4d9477887849981a5e39ebf9520024a049c1ffa
tracer-ics2016 <tracer@njuics.org>
2017-01-06 19:04:13 +0800

    >  compile game 151220100 tangruize Linux 128879787e4d 3.19.0-32-generic #37~14.04.1-Ubuntu SMP Thu Oct 22 09:41:40 UTC 2015 x86_64 GNU/Linux  19:04:13 up 1 day,  2:24,  1 user,  load average: 0.30, 0.46, 0.40 b0dda6f9da7032c9f2bfeb49e19f5c0f7b6ffcaa

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -79,7 +79,9 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
 			return 1;
 		}
 		else if (query_key(i) == KEY_STATE_RELEASE) {
-			
+			key_release_callback(get_keycode(i));
+			clear_key(i);
+			return 1;
 		}
 	}
 	//assert(0);
